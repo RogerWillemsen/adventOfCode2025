51dab3a1fda63712f37652c0aec07a2df493d79b
Roger Willemsen <rogerwillemsen@Rogers-MBP.home>
2025-12-04 09:55:15 +0100

    day 4

---
+++ day4/day4.py
@@ -0,0 +1,49 @@
+import numpy as np
+import time
+
+def perform_convolution(matrix, kernel):
+    number_rows, number_columns = matrix.shape
+    padded = np.pad(matrix, (1, 1))
+    result = np.zeros((number_rows, number_columns))
+    for i in range(number_rows):
+        for j in range(number_columns):
+            region = padded[i:i + 3, j:j + 3]
+            result[i, j] = np.sum(region * kernel)
+
+    return result
+
+with open('input.txt') as f:
+    lines = f.read().splitlines()
+
+start = time.perf_counter()
+
+number_rows = len(lines[0])
+number_columns = len(lines)
+can_access2 = 0
+kernel = np.ones((3,3))
+
+matrix = np.zeros((number_rows,number_columns))
+for i in range(number_rows):
+    for j in range(number_columns):
+        if lines[i][j] == '@':
+            matrix[i, j] = 1
+
+# Part 1
+result = perform_convolution(matrix, kernel)
+can_access1 = np.sum((result < 5) * matrix)
+
+# # Part 2
+loop_counter = 0
+while True:
+    result = perform_convolution(matrix, kernel)
+    new_access = np.sum((result < 5) * matrix)
+    can_access2 += new_access
+    matrix = (result >= 5) * matrix
+    loop_counter += 1
+    if new_access == 0:
+        break
+
+print("Can access part 1: %d" % can_access1)
+print("Can access part 2: %d" % can_access2)
+print("Number of iterations taken: %d" % loop_counter)
+print("Time: %.6f" % (time.perf_counter() - start))
